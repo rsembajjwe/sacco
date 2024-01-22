7a4e087e7810bb876c66ee73431ad1834662966c
start.vaadin.com <no-reply@vaadin.com>
2024-01-22 16:13:01 +0000

    Generated project

---
+++ src/main/java/com/methaltech/application/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.methaltech.application.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/methaltech/application/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.methaltech.application.services;
+
+import org.springframework.lang.NonNullApi;
